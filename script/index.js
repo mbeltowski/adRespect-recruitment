300b2ee527defb10f198fb46585254f0f513ecab
mbeltowski <marcel.beltowski@gmail.com>
2023-08-22 18:15:54 +0200

    styles and section changes, animations

--- script/index.js
+++ script/index.js
@@ -1,35 +1,25 @@
-// import Masonry from "masonry-layout"
-
-window.onload = () => {
-	const imgContainer = document.querySelector(".images-container")
-	const randomImages = ["https://picsum.photos/531/320", "https://picsum.photos/775/220", "https://picsum.photos/112/500", "https://picsum.photos/663/404", "https://picsum.photos/991/702", "https://picsum.photos/252/301"]
-	randomImages.forEach((img) => {
-		const imgElement = document.createElement("img")
-		console.log(imgElement)
-		imgElement.src = img
-		imgElement.classList.add("border-4")
-		imgContainer.appendChild(imgElement)
-	})
-
-	const mansory = new Masonry(imgContainer, {
-		itemSelector: ".images-container img",
-		// columnWidth: 200,
-		gutter: 10,
-		fitWidth: true,
-		stagger: 30
-		// transitionDuration: "0.2s"
-		// percentPosition: true
-		// horizontalOrder: true
-	})
-}
-
 const menuToggle = document.getElementById("menu-toggle")
 const menu = document.getElementById("menu")
 
 menuToggle.addEventListener("change", () => {
-	if (menuToggle.checked) {
-		menu.classList.remove("hidden")
-	} else {
-		menu.classList.add("hidden")
-	}
+	menu.classList.toggle("hidden")
+})
+
+const sliderLeft = document.getElementById("slider-left")
+const sliderRight = document.getElementById("slider-right")
+
+let imgIndex = 0
+
+const images = ["files/maze.png", "https://picsum.photos/531/500", "https://picsum.photos/500/520", "https://picsum.photos/450/500", "https://picsum.photos/500/404"]
+
+sliderLeft.addEventListener("click", () => {
+	const currentImg = document.querySelector("#slider-image")
+	imgIndex = Math.max(imgIndex - 1, 0)
+	currentImg.src = images[imgIndex]
+})
+
+sliderRight.addEventListener("click", () => {
+	const currentImg = document.querySelector("#slider-image")
+	imgIndex = Math.min(imgIndex + 1, images.length - 1)
+	currentImg.src = images[imgIndex]
 })
